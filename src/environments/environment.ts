36673c165004aed5a306aa9a7f953da8e2582f47
Swaroopp15 <swarooppalacharla05@gmail.com>
2025-08-25 18:53:08 +0530

    Add a file to deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    production: false,
-    backendUrl: 'http://localhost:5000'
+    production: true,
+    backendUrl: 'https://chatspace-backend-uhnn.onrender.com/'
 };
